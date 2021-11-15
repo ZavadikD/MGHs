515af0a370e32ef0c1888f032b53a01342c60662
ZavadikD <94398579+ZavadikD@users.noreply.github.com>
2021-11-15 21:37:27 +0200

    Add files via upload

---
+++ Official_qpcr_analysis.R
@@ -0,0 +1,69 @@
+library(pcr)
+library(ggplot2)
+library(ggpubr)
+library(RColorBrewer)
+setwd('/home/zavadska/Documents/biology/all_chlorophyta_its2/Konstanz-wetlab part/concentrated')
+
+######pcr package estimates; formulas/explanation in Supplement#####
+
+###put the directory with CT techical replicate means below.
+ct1 <- read.csv("Official_CONC_forCTvalue_analysis_means.csv", sep = ",")
+
+###making a list of genes, conditions and subsetting a dataframe to get rid of C.reinhardtii CT values
+group_var <- rep(c("Nitrate deficiency","Standard culture","Darkness-light","Darkness"), each = 3)
+ct1 <- ct1[1:12,]
+genes <- c("mer3","hop1","hop2","mnd1")
+
+# calculate all values and errors in one step
+## mode == 'separate_tube' default
+resi <- data.frame()
+for(i in genes){
+  resa <- pcr_analyze(ct1[,c(which(colnames(ct1) == i), which(colnames(ct1) == paste0('L5.',i)))],
+                     group_var = group_var,
+                     reference_gene = paste0('L5.',i),
+                     reference_group = 'Standard culture')
+  print(resa)
+  resi <- rbind(resi, resa)
+}
+resi
+#########for significance test by lm(multiple conditions)#########
+resstati <- data.frame()
+for(i in genes){
+  resstata <- pcr_test(ct1[,c(which(colnames(ct1) == i), which(colnames(ct1) == paste0('L5.',i)))],
+                     group_var = group_var,
+                     reference_gene = paste0('L5.',i),
+                     reference_group = 'Standard culture',
+                  test = 'lm')
+  print(resstata)
+  resstati <- rbind(resstati, resstata)
+}
+
+resstati 
+
+#now, the "resstati" and "resi" tables require a bit of reshaping - for more convenient plotting. Those tables, reshaped and combined together, are found in "ddCT_calc_res_publish.csv" - see "plotting"
+
+###plotting
+res <- read.csv("Official_ddCT_calc_res_publish.csv", sep = ",")
+str(res)
+res$p_value_factor <-  c()
+
+res1 <- within(res,{p_value_factor <- NA
+p_value_factor[p_value <= 0.05] <- "Significant"
+p_value_factor[p_value >= 0.05] <- "Not significant" 
+p_value_factor[p_value == "No"] <- "Control"
+})
+
+res1
+
+ggplot(data=res1, aes(x=factor(group, levels = c("Standard culture","Nitrate deficiency","Darkness","Darkness-light")), y=relative_expression, group=gene, color = gene, shape = p_value_factor)) +
+  #geom_point(size=5, position = position_dodge(width=0.5))+
+  yscale("log2", .format = TRUE)+ 
+  geom_line(position = position_dodge(width=0.5))+
+  geom_pointrange(aes(ymin=as.numeric(relative_expression-error), ymax=as.numeric(relative_expression+error)),size=0.8, position = position_dodge(width=0.5))+
+  #geom_errorbar(aes(ymin=as.numeric(relative_expression-error), ymax=as.numeric(relative_expression+error)), size=0.8, position = position_dodge(width=0.5))+
+  xlab("Culture conditions") + ylab("Relative mRNA expression") +
+  labs( color = "Gene", shape = "Difference control \nvs test condition")+ scale_color_brewer(palette = "Paired")+theme_bw(base_size = 15)+
+  scale_shape_manual(values=c(7:10))
+
+
+
